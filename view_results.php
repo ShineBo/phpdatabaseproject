8875af7fe939cf4bcb4074de94bcc6f4ff4ee964
Shine <shinebobo648@gmail.comm>
2023-08-30 10:20:25 +0630

    third

--- view_results.php
+++ view_results.php
@@ -5,10 +5,15 @@ if (!isset($_SESSION["user_id"]) || !$_SESSION["is_admin"]) {
     exit();
 }
 
-require 'db.php'; // Include the database connection
+require 'db.php'; // Make sure to include your database connection
 
-// Fetch and display voting results here
+// Fetch and display voting results
+$query = "SELECT r.id, r.name, COUNT(v.id) AS vote_count FROM representatives r
+          LEFT JOIN votes v ON r.id = v.representative_id
+          GROUP BY r.id, r.name";
+$result = $conn->query($query);
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -18,11 +23,34 @@ require 'db.php'; // Include the database connection
 <body>
     <div class="container mt-5">
         <h2>View Voting Results</h2>
-        <!-- Display voting results here -->
-        <form action="admin_dashboard.php" method="post">
-            <button type="submit" class="btn btn-secondary">Dashboard</button>
-        </form>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Representative ID</th>
+                    <th>Representative Name</th>
+                    <th>Vote Count</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row["id"] . "</td>";
+                        echo "<td>" . $row["name"] . "</td>";
+                        echo "<td>" . $row["vote_count"] . "</td>";
+                        echo "</tr>";
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>No voting results available.</td></tr>";
+                }
+                ?>
+            </tbody>
+            <form action="admin_dashboard.php" method="post">
+                <button type="submit" class="btn btn-secondary">Dashboard</button>
+            </form>
+        </table>
     </div>
-
 </body>
 </html>
+
